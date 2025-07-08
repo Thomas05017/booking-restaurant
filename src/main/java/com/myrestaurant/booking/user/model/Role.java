b9b1ac56481b2735283639b84f89cd11c63859e4
Thomas Franchi <t.franchi@itsprodigi.it>
2025-07-08 12:35:17 +0200

    Initial commit

---
+++ src/main/java/com/myrestaurant/booking/user/model/Role.java
@@ -0,0 +1,14 @@
+package com.myrestaurant.booking.user.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
